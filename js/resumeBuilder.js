384b693f98ef2fc52bba2357fc8d8aee4a712ecd
gwdavis <computer@davisfamily.com>
2016-02-29 14:06:50 -0500

    New Branch for Javascript Design Patterns
    refactoring for MVC pattern

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,7 +5,8 @@
 by Gary W. Davis
 */
  
- var bio = {
+var model = {
+	bio:  {
  	"name" : "Gary W. Davis",
  	"role" : "Treasurer",
  	"contacts" : {
@@ -16,9 +17,9 @@ by Gary W. Davis
  	"cell" : "+1-914-419-9788",
  	"photo" : "http://s.gravatar.com/avatar/2c4d8d5e413f44946391fc134d28e04a?s=80",
 	"skills" : ["WordPress", "Python"]
- };
+ },
 
- var work = {
+	work: {
  	"jobs" : [
  	{
  		"title" : "Consultant",
@@ -35,9 +36,9 @@ by Gary W. Davis
 	 	"description" : ""
 	 }
  	]
- };
+ },
 
- var education = {
+ 	education: {
  	"schools" : [
  	{
  		"name" : "Harvard Business School",
@@ -62,9 +63,9 @@ by Gary W. Davis
  		"url" : ""
  	}
  	]
- };
+ },
 
-var projects = {
+	projects: {
  	"projects" : [
  	{
  		"title" : "Project 1",
@@ -73,104 +74,137 @@ var projects = {
  		"image" : "images/RowingRegattaApp.png"
  	}
  	]
-};
-
-bio.display = function(){
-	var formattedName = HTMLheaderName.replace("%data%",bio.name);
-	var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-	var formattedBioPic = HTMLbioPic.replace('%data%',bio.photo);
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-	$('#header').prepend(formattedBioPic);
-
-	if(bio.skills.length > 0){
-	 	$("#header").append(HTMLskillsStart);
-	 	for (i in bio.skills){
-	 		var formattedSkills = HTMLskills.replace("%data%",bio.skills[i]);
-	 		$("#skills").append(formattedSkills)
-	 	}
 	}
 };
 
-work.display = function (){
- 	if(work.jobs.length > 0){
- 	// console.log(work.jobs.length)
-	 	for (i in work.jobs){
-	 		$("#workExperience").append(HTMLworkStart);
-	 		var formattedWorkEmployer = HTMLworkEmployer.replace("%data%",work.jobs[i].employer);
-	 		var formattedWorkTitle = HTMLworkTitle.replace("%data%",work.jobs[i].title);
-	 		var formattedWorkEmployerTitle = formattedWorkEmployer + formattedWorkTitle
-	 		var formattedWorkDescription = HTMLworkDescription.replace("%data%",work.jobs[i].description)
-	 		var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[i].location)
-	 		var formattedWorkDates = HTMLworkDates.replace("%data%",work.jobs[i].dates)
-	 		$(".work-entry:last").append(formattedWorkEmployerTitle)
-	 		$(".work-entry:last").append(formattedWorkLocation)
-	 		$(".work-entry:last").append(formattedWorkDates)
-	 		$(".work-entry:last").append(formattedWorkDescription)
- 		}
- 	}
+var cntlr = {
+	
+	getBio: {
+		"formattedName": HTMLheaderName.replace("%data%",model.bio.name),
+		"formattedRole": HTMLheaderRole.replace("%data%",model.bio.role),
+		"formattedBioPic": HTMLbioPic.replace('%data%',model.bio.photo),
+		"formattedSkills": function() {
+			if(model.bio.skills.length > 0) {
+				var formattedSkills = [];
+				for (i in model.bio.skills){
+					formattedSkills.push(HTMLskills.replace("%data%",model.bio.skills[i]));
+				};
+				}
+			return formattedSkills;
+			}()
+		
+	},
+	
+	getWork: function() {
+		if(model.work.jobs.length > 0){
+			var workExperience = [];
+			for (i in model.work.jobs){
+				var job = {};
+				job.formattedWorkEmployer = HTMLworkEmployer.replace("%data%",model.work.jobs[i].employer);
+				job.formattedWorkTitle = HTMLworkTitle.replace("%data%",model.work.jobs[i].title);
+				job.formattedWorkEmployerTitle = job.formattedWorkEmployer + job.formattedWorkTitle
+				job.formattedWorkDescription = HTMLworkDescription.replace("%data%",model.work.jobs[i].description)
+				job.formattedWorkLocation = HTMLworkLocation.replace("%data%",model.work.jobs[i].location)
+				job.formattedWorkDates = HTMLworkDates.replace("%data%",model.work.jobs[i].dates)
+				workExperience.push(job);
+ 			}
+		}
+		return workExperience;
+	},
+	
+	/* Internationalize Names
+	Includes a button from helper.js and a function that capitalizes the last name
+	*/
+
+	inName: function(name) {
+		// Splits a two word name, capitalizing the first name and putting second name in all caps
+		var splitName = name.trim().split(" ");
+		//first name to lowercase and first letter in caps
+		var fname = splitName[0][0].toUpperCase() + splitName[0].slice(1).toLowerCase()
+		//last name to all upper case
+		var lname = splitName[1].toUpperCase();
+		//combine names
+		intName = fname + " " + lname;
+		return intName;
+	}
+
+
+//
+//projects.display = function(){
+//	if(projects.projects.length >0){
+//		console.log(projects.projects.length);
+//		for (i in projects.projects){
+//			$('#projects').append(HTMLprojectStart);
+//			var formattedProjectsTitle = HTMLprojectTitle.replace('%data%', projects.projects[i].title);
+//			var formattedProjectsDates = HTMLprojectDates.replace('%data%', projects.projects[i].dates);
+//			var formattedProjectsDescription = HTMLprojectDescription.replace('%data%', 				   projects.projects[i].description);
+//			var formattedProjectsImage = HTMLprojectImage.replace('%data%', projects.projects[i].image);
+//			$('.project-entry:last').append(formattedProjectsTitle);
+//			$('.project-entry:last').append(formattedProjectsDates);
+//			$('.project-entry:last').append(formattedProjectsDescription);
+//			$('.project-entry:last').append(formattedProjectsImage);
+//		}
+//	}
+
+//
+//
+
 };
 
-projects.display = function(){
-	if(projects.projects.length >0){
-		console.log(projects.projects.length);
-		for (i in projects.projects){
-			$('#projects').append(HTMLprojectStart);
-			var formattedProjectsTitle = HTMLprojectTitle.replace('%data%', projects.projects[i].title);
-			var formattedProjectsDates = HTMLprojectDates.replace('%data%', projects.projects[i].dates);
-			var formattedProjectsDescription = HTMLprojectDescription.replace('%data%', projects.projects[i].description);
-			var formattedProjectsImage = HTMLprojectImage.replace('%data%', projects.projects[i].image);
-			$('.project-entry:last').append(formattedProjectsTitle);
-			$('.project-entry:last').append(formattedProjectsDates);
-			$('.project-entry:last').append(formattedProjectsDescription);
-			$('.project-entry:last').append(formattedProjectsImage);
+var view = {
+	displayBio: function() {
+		var formattedRole = cntlr.getBio.formattedRole,
+			formattedName = cntlr.getBio.formattedName,
+			formattedBioPic = cntlr.getBio.formattedBioPic,
+			formattedSkills = cntlr.getBio.formattedSkills;
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+		$('#header').prepend(formattedBioPic);
+		if(formattedSkills.length > 0){
+			$("#header").append(HTMLskillsStart);
+			$("#skills").append(formattedSkills);
+		}
+	},
+	displayWork: function() {
+		if(cntlr.getWork().length > 0){
+			for (var i=0; i< cntlr.getWork().length; i++){
+				$("#workExperience").append(HTMLworkStart);
+				$(".work-entry:last").append(cntlr.getWork()[i].formattedWorkEmployerTitle)
+				$(".work-entry:last").append(cntlr.getWork()[i].formattedWorkLocation)
+				$(".work-entry:last").append(cntlr.getWork()[i].formattedWorkDates)
+				$(".work-entry:last").append(cntlr.getWork()[i].formattedWorkDescription)
+			}
 		}
 	}
-};
-
-
-/* Internationalize Names
-Includes a button from helper.js and a function that capitalizes the last name
-*/
-
-function inName (name) {
-	// Splits a two word name, capitalizing the first name and putting second name in all caps
-	var splitName = name.trim().split(" ");
-	//first name to lowercase and first letter in caps
-	var fname = splitName[0][0].toUpperCase() + splitName[0].slice(1).toLowerCase()
-	//last name to all upper case
-	var lname = splitName[1].toUpperCase();
-	//combine names
-	intName = fname + " " + lname;
-	return intName;
+	
 };
 
 
 // Prints out locations worked in work object as passed to the function
-// 
-// function locationizer(work_obj){
-// 	var locationsArray = []
-// 	for (i in work_obj){
-// 		var workLocations = work_obj[i].location
-// 		locationsArray.push(workLocations)
-// 	}
-// 	return locationsArray
-// }
+ 
+ function locationizer(work_obj){
+ 	var locationsArray = []
+ 	for (i in work_obj){
+ 		var workLocations = work_obj[i].location
+ 		locationsArray.push(workLocations)
+ 	}
+ 	return locationsArray
+ }
 
 
 // Prints to the log the location of clicks on the page
-// 
-// $(document).click(function(loc) {
-//   var x = loc.pageX;
-//   var y = loc.pageY;
-//   logClicks(x,y);
-// });
+ 
+ $(document).click(function(loc) {
+   var x = loc.pageX;
+   var y = loc.pageY;
+   logClicks(x,y);
+ });
 //////////////////////////////////MAIN////////////////////////////////////////
-bio.display();
-work.display();
-projects.display();
+view.displayBio();
+view.displayWork();
+//projects.display();
 $("#Main").append(internationalizeButton);
-$('#mapDiv').append(googleMap);
-console.log(inName("gary davis"));
+//$('#mapDiv').append(googleMap);
+console.log(cntlr.inName("gary davis"));
 
  
